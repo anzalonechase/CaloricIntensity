612861cd49fc0a62d96d421e491b606308b1ca53
Peter Estrada Ledesma <pestradaledesm8169@sdsu.edu>
2021-11-26 17:16:17 -0800

    Created player controller script that controls players movement. Also had to tweak the limbs bc they were in the wrong position

--- Assets/Player_Controller.cs
+++ Assets/Player_Controller.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class Player_Controller : MonoBehaviour
 {
+
+    public float moveSpeed = 10f;
+
+
+
+    public CharacterController controller;      // Set in inspector 
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +20,12 @@ public class Player_Controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;   //calculates movement
+
+        controller.Move(move * moveSpeed * Time.deltaTime);   //Applies movement
+
     }
 }
