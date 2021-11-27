f17ea7a31a5582b0991c5f70cc57e3aaecc4238a
Peter Estrada Ledesma <pestradaledesm8169@sdsu.edu>
2021-11-26 21:44:16 -0800

    Added a empty gameobject parent to the left aim control adn right aim control. Since there will be two blaster I have to make sure that the scirpt handles that so the script that hnadles this will be inthe parent and i renamed the aim left and aim right to burger spawn point adn hotdog spawn point

--- Assets/Resources/Scripts/Aim_Controller.cs
+++ Assets/Resources/Scripts/Aim_Controller.cs
@@ -8,6 +8,16 @@ public class Aim_Controller : MonoBehaviour
     public Transform fireSpawn;       // Set both in inspector        (This is where bullet comes from)
     public Rigidbody bullet;           //                              (This moves the bullet)
 
+
+
+    public Transform burgerSpawn;       // Set both in inspector        (This is where burger comes from)
+    public Rigidbody burger;           //                              (This moves the burger)
+
+    public Transform hotdogSpawn;       // Set both in inspector        (This is where hotdog comes from)
+    public Rigidbody hotDog;           //                              (This moves the hotdog)
+
+
+
     public float force;       //WIll need to give it force
 
     void Start()
@@ -26,6 +36,14 @@ public class Aim_Controller : MonoBehaviour
 
             bulletInstance.AddForce(fireSpawn.forward * force);         // Shoots out the bullet
         }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            var bulletInstance = Instantiate(bullet, fireSpawn.position, fireSpawn.rotation);        //creates an instance of bullet in fireSpawn positon
+
+            bulletInstance.AddForce(fireSpawn.forward * force);         // Shoots out the bullet
+        }
+
 
     }
 }
