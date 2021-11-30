2fa2add83207f34494f81f27b4c21e4bb37a8d50
Peter Estrada Ledesma <pestradaledesm8169@sdsu.edu>
2021-11-29 19:32:53 -0800

    Created a text box above the NPC, it displays its order. I coded the NPC script so that it gives a random order of hotdogs adn burgers. Sometimes no burgres only hotdogs sometimes the other way around. but since it is random it will never be the same. I fixed it so that when NPC is shot by those it needs one less of them.

--- Assets/Resources/Scripts/NPC_Controller.cs
+++ Assets/Resources/Scripts/NPC_Controller.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class NPC_Controller : MonoBehaviour
 {
@@ -16,17 +17,57 @@ public class NPC_Controller : MonoBehaviour
 
 
 
-    public Text txt;                  // To display box above NPC              // (Both set in inspector)
+    public TextMeshPro  order;  // references teh text above NPC  
 
-    public Transform positionText;  // WIll reference where the position of the text should be
+    public int numHotdogs = 0;     // How many hotdogs will the npc want 
+    public int numBurgers = 0;    // How many burgers will the npc want 
 
 
 
-    
+
+
+
+
+
+
+
+
 
     void Start()
     {
-        rb =GameObject.Find("NPC").GetComponent<Rigidbody>();
+        rb = GameObject.Find("NPC").GetComponent<Rigidbody>();
+         
+
+
+
+
+
+
+
+
+
+        order = GameObject.Find("Order").GetComponent<TextMeshPro>();   // Gets reference to the text above NPC
+
+        while(numBurgers == 0 & numBurgers == 0)   // This makes sure that both the hotdog and burger count are never both 0
+        {
+            numHotdogs = Random.Range(0, 4);  // 0, 1, 2, or 3
+            numBurgers = Random.Range(0, 4);
+
+        }
+
+        order.text = (numBurgers + "  Burgers\n" + numHotdogs + "  Hotdogs");       // Puts out the order to NPC text box
+
+
+
+
+
+
+
+
+
+
+
+
 
         moveSpeed = 400;
 
@@ -36,24 +77,18 @@ public class NPC_Controller : MonoBehaviour
     
     void Update()
     {
-
-
-
-
-
-        Vector3 namePos = Camera.main.WorldToScreenPoint(positionText.position);         // Puts text right above NPC head
-        txt.transform.position = namePos;
-
-
-
-
-
-
         rb.velocity = transform.forward * Time.deltaTime * moveSpeed;         // Makes it move 
+
+
+
+        order.text = (numBurgers + "  Burgers\n" + numHotdogs + "  Hotdogs");       // Puts out the order to NPC text box REMAINING
+
+
     }
 
     public void OnCollisionEnter(Collision collision)
     {
+
         if(gameObject.tag == "NPC" && collision.gameObject.tag =="Boundary" )
         {
           
@@ -62,5 +97,29 @@ public class NPC_Controller : MonoBehaviour
             flipBy += 180;   // TO make sure that next time the rotation is set, it will go exactly the opposite way
 
         }
+
+
+
+
+
+
+
+
+
+
+        if (gameObject.tag == "NPC" && collision.gameObject.tag == "Burger")          // If hit by a burger decrease th burger order
+        {
+            numBurgers -= 1;
+        }
+
+        if (gameObject.tag == "NPC" && collision.gameObject.tag == "Hotdog")          // If hit by a hotdog decrease the hotdog order
+        {
+            numHotdogs -= 1;
+        }
+
+
+
+
+
     }
 }
