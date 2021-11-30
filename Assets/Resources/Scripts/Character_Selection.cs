420b07c3d0f2d3eaac2defd0e7f02b7d14d772cc
Arman Shahriari <ashahriari9557@sdsu.edu>
2021-11-29 20:09:50 -0800

    Added quit button to the scene

--- Assets/Resources/Scripts/Character_Selection.cs
+++ Assets/Resources/Scripts/Character_Selection.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class Character_Selection : MonoBehaviour
 {
     private Button backBtn;
+    private Button quitBtn;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,10 +23,27 @@ public class Character_Selection : MonoBehaviour
     {
         backBtn = GameObject.Find("BackBtn").GetComponent<Button>();
         backBtn.onClick.AddListener(delegate { LoadSceneByNumber(0); });
+
+        quitBtn = GameObject.Find("QuitBtn").GetComponent<Button>();
+        quitBtn.onClick.AddListener(delegate { exitFromtheEditor(); });
     }
     private void LoadSceneByNumber(int sceneNumber)
     {
         SceneManager.LoadScene(sceneNumber);
+    }
+
+    /* 
+    * When user click the exit button. it exit from editor, and play mode
+    */
+    private void exitFromtheEditor()
+    {
+        // save any game data here
+        #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+        #else
+                         Application.Quit();
+                         Application.Quit();
+        #endif
     }
 
 }
